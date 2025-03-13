b8d53ed33ca1b6cd9565bc2821f9b90d4ad9ef03
winrid <winrid@gmail.com>
2025-03-13 11:37:43 -0700

    progress

--- libraries/sdk/src/main/java/com/fastcomments/sdk/FastCommentsSDK.java
+++ libraries/sdk/src/main/java/com/fastcomments/sdk/FastCommentsSDK.java
@@ -2,37 +2,23 @@ package com.fastcomments.sdk;
 
 import android.content.Context;
 
+import com.fastcomments.core.CommentWidgetConfig;
 import com.fastcomments.model.APICommentPublicComment;
+import com.fastcomments.model.UserSessionInfo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FastCommentsSDK {
 
-    private static volatile FastCommentsSDK instance;
     private Context context;
-    private User currentUser;
-    private SSOOptions ssoOptions;
+    private UserSessionInfo currentUser;
+    private CommentWidgetConfig config;
 
-    private FastCommentsSDK(Context context) {
+    public FastCommentsSDK(Context context) {
         this.context = context;
     }
 
-    public static FastCommentsSDK init(Context context) {
-        if (instance == null) {
-            synchronized (FastCommentsSDK.class) {
-                if (instance == null) {
-                    instance = new FastCommentsSDK(context);
-                }
-            }
-        }
-        return instance;
-    }
-    public void fetchComments(int page, CommentsCallback callback) {
-        callback.onCommentsFetched(comments);
-    }
-
-    public interface CommentsCallback {
-        void onCommentsFetched(List<APICommentPublicComment> comments);
+    public void loadForConfig(CommentWidgetConfig config) {
+        this.config = config;
     }
 }
--- libraries/sdk/src/main/java/com/fastcomments/sdk/FastCommentsView.java
+++ libraries/sdk/src/main/java/com/fastcomments/sdk/FastCommentsView.java
@@ -6,26 +6,34 @@ import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.fastcomments.client.R;
+import com.fastcomments.core.CommentWidgetConfig;
+
 import java.util.List;
 
 public class FastCommentsView extends LinearLayout {
 
     private RecyclerView recyclerView;
     private CommentsAdapter adapter;
+    private CommentWidgetConfig config;
     private int currentPage = 1;
 
-    public FastCommentsView(Context context) {
+    public FastCommentsView(Context context, CommentWidgetConfig config) {
         super(context);
+        this.config = config;
         init(context);
     }
 
-    public FastCommentsView(Context context, AttributeSet attrs) {
+    public FastCommentsView(Context context, AttributeSet attrs, CommentWidgetConfig config) {
         super(context, attrs);
+        this.config = config;
         init(context);
     }
 
-    public FastCommentsView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public FastCommentsView(Context context, AttributeSet attrs, int defStyleAttr, CommentWidgetConfig config) {
         super(context, attrs, defStyleAttr);
+        this.config = config;
         init(context);
     }
 
@@ -42,12 +50,7 @@ public class FastCommentsView extends LinearLayout {
     }
 
     private void loadComments(int page) {
-        FastCommentsSDK.init(getContext()).fetchComments(page, new FastCommentsSDK.CommentsCallback() {
-            @Override
-            public void onCommentsFetched(List<Comment> comments) {
-                adapter.setComments(comments);
-            }
-        });
+        new FastCommentsSDK(getContext()).loadForConfig(config);
     }
 
     // Public method to load a specific page.
