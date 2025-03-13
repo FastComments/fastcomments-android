a8328ab06fed6e629f0daeb4a82536017f250e3b
winrid <winrid@gmail.com>
2025-03-13 15:06:36 -0700

    progress

--- app/src/main/java/com/fastcomments/MainActivity.kt
+++ app/src/main/java/com/fastcomments/MainActivity.kt
@@ -17,25 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            FastcommentsexamplesimpleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        val sdk = FastCommentsSDK.init(this)
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
