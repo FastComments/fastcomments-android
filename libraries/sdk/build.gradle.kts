13a0a9402ca7b1bbdfe90db3b072273420a73c16
winrid <winrid@gmail.com>
2025-03-14 20:29:22 -0700

    now compiles

--- libraries/sdk/build.gradle.kts
+++ libraries/sdk/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     // FastComments Java client
     implementation(libs.fastcommentsCore)
     implementation(libs.fastcommentsClient)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.12.1")
 
     testImplementation(libs.junit)
