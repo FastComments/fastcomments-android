e297fd833acfa5e3bc8d477354b1ae85393cdce7
winrid <winrid@gmail.com>
2025-03-18 21:54:09 -0700

    adding pubsub

--- refresh_fc_deps.sh
+++ refresh_fc_deps.sh
@@ -2,3 +2,4 @@
 
 ./gradlew build --refresh-dependencies -Pdependency=com.fastcomments:core:0.0.1
 ./gradlew build --refresh-dependencies -Pdependency=com.fastcomments:client:0.0.1
+./gradlew build --refresh-dependencies -Pdependency=com.fastcomments:pubsub:0.0.1
