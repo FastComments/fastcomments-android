a4c5fca943110d072009a70828ec200050c2938c
winrid <winrid@gmail.com>
2025-03-26 23:52:19 -0700

    start of publishing

--- libraries/sdk/build.gradle.kts
+++ libraries/sdk/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -33,6 +35,12 @@ android {
             pickFirsts.add("META-INF/LICENSE.md")
         }
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -53,4 +61,68 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+val releaseVersion = findProperty("releaseVersion") as String? ?: "0.0.1-SNAPSHOT"
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.fastcomments"
+            artifactId = "android-sdk"
+            version = releaseVersion
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name.set("FastComments Android SDK")
+                description.set("Official FastComments Android SDK")
+                url.set("https://github.com/FastComments/fastcomments-android")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("fastcomments")
+                        name.set("FastComments Team")
+                        email.set("support@fastcomments.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/FastComments/fastcomments-android.git")
+                    developerConnection.set("scm:git:ssh://github.com/FastComments/fastcomments-android.git")
+                    url.set("https://github.com/FastComments/fastcomments-android")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri(
+                if (releaseVersion.endsWith("SNAPSHOT")) {
+                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                } else {
+                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                }
+            )
+            credentials {
+                username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? = findProperty("signingKey") as String? ?: System.getenv("SIGNING_KEY")
+    val signingPassword: String? = findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["release"])
 }
